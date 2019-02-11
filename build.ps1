b4ad29226878109a672b79d1e163d4d7ed41f25c
Jesper Matthiesen <jm@evosep.com>
2019-02-11 15:22:09 +0100

    use docker ENV and ARG directives for parameterizing JENKINS_VERSION, etc

--- build.ps1
+++ build.ps1
@@ -13,5 +13,5 @@ docker build -t asmagin/jenkins-on-windowsservercore:slave        .\slave
 
 $JENKINS_VERSION = "2.46.1"
 
-docker build -t asmagin/jenkins-on-windowsservercore:$JENKINS_VERSION             .\master
+docker build --build-arg JENKINS_VERSION=$JENKINS_VERSION -t asmagin/jenkins-on-windowsservercore:$JENKINS_VERSION             .\master
 docker build -t asmagin/jenkins-on-windowsservercore:$JENKINS_VERSION-slave       .\slave

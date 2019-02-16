e7755ad9a24924b8e45d54ac872990db46b43faa
Jesper Matthiesen <bruder@bruder.dk>
2019-02-16 17:44:57 +0100

    Numerous small fixes/improvements (#9)
    * Fix instructions on building from Dockerfile
    
    Remove bad parenthesis from end of lines.
    Add missing ".git" to repository paths.
    Change quotes to doubles (Docker doesn't understand the single quotes, at least not in a plain command prompt).
    
    * Set security protocol to tsl11/tsl12 for web requests
    
    Powershell/.Net framework may default to tls1.0 which causes the https downloads to fail (SSL/TLS secure channel error).
    
    * Fix SecurityProtocol parse error due to double quotes
    
    Also limit to TLS1.2 which is used by the referenced websites - older protocols are unsafe.
    
    * fix script failing in case no containers or dangling images present
    
    * use docker ENV and ARG directives for parameterizing JENKINS_VERSION, etc
    
    * set base image to openjdk 8; Jenkins does not support later versions
    
    * transplant base image, TLS settings and parameterization for Jenkins/Git over from master/Dockerfile

--- build.ps1
+++ build.ps1
@@ -1,15 +1,17 @@
 Clear-Host
 
-$ErrorActionPreference = "STOP"
+$ErrorActionPreference = 'Continue'
 
 # Note: run clean-up
 docker rm $(docker ps -a -q)
 docker rmi $(docker images -f dangling=true -q)
+
+$ErrorActionPreference = 'Stop'
 
 docker build -t asmagin/jenkins-on-windowsservercore              .\master
 docker build -t asmagin/jenkins-on-windowsservercore:slave        .\slave
 
 $JENKINS_VERSION = "2.46.1"
 
-docker build -t asmagin/jenkins-on-windowsservercore:$JENKINS_VERSION             .\master
-docker build -t asmagin/jenkins-on-windowsservercore:$JENKINS_VERSION-slave       .\slave
+docker build --build-arg JENKINS_VERSION=$JENKINS_VERSION -t asmagin/jenkins-on-windowsservercore:$JENKINS_VERSION             .\master
+docker build --build-arg JENKINS_VERSION=$JENKINS_VERSION -t asmagin/jenkins-on-windowsservercore:$JENKINS_VERSION-slave       .\slave
